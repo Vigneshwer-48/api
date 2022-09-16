ba8799881481df318dc086372cf735e91a2c2ebf
Vigneshwer-48 <91060970+Vigneshwer-48@users.noreply.github.com>
2022-09-16 14:05:45 +0530

    new change
    new change

--- TASK/TASK 1/api.js
+++ TASK/TASK 1/api.js
@@ -1,3 +1,18 @@
+
+var object1 = {
+    name: 'Person 1',
+    age: '5'
+  };
+      
+  var object2 = {
+    age: '5',
+    name: 'Person 2'
+  };
+  JSON.stringify(object1) === JSON.stringify(object2)
+      
+  console.log(object1, object2);
+  
+
 var request=new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v2/all");
 request.send();
